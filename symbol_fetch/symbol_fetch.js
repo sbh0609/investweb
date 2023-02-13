c79e75fc90e622c2507d824b9aecb2d1a486505b
sbh0609 <49151146+sbh0609@users.noreply.github.com>
2023-02-13 18:37:19 +0900

    띄어쓰기 수정

--- symbol_fetch/symbol_fetch.js
+++ symbol_fetch/symbol_fetch.js
@@ -9,23 +9,23 @@ const conn = JSON.parse(fs.readFileSync("SoloData/SoloData.json"));
 let connection = createConnection(conn); // DB 커넥션 생성
 
 connection.connect((err) => {
-  if (err) {console.log("stock 테이블 저장 실패");
+  if (err) {console.log("연결실패");
   console.log(err);
   }
   else {
     let create_sql = `CREATE TABLE stock ( 
-      ${stock.columns.join(`VARCHAR(50),
+      ${stock.columns.join(` VARCHAR(50),
       `)} VARCHAR(50)
       );`;
     connection.query(create_sql, function (err, results) {
       //create문이 에러면 로그 띄우기
-      if(err) console.log("'stock' 테이블 이미 있음");
+      if(err) console.log(err);
       else console.log("stock 테이블 생성");
       //테이블 생성,초기화 이후 데이터 저장
       var insert_sql = `INSERT INTO stock values ?;`;
       //db에 데이터 저장
       connection.query(insert_sql, [stock.data], function (err, results) {
-        if (err) console.log(err);
+        if (err) console.log("ha");
         else console.log("stock 테이블 정보 저장 완료");
         connection.end();
       });
