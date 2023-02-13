780056f2c74889b6b2f8cf81679b28a105746bd9
sbh0609 <49151146+sbh0609@users.noreply.github.com>
2023-02-13 18:50:48 +0900

    stock 존재 시 delete

--- symbol_fetch/symbol_fetch.js
+++ symbol_fetch/symbol_fetch.js
@@ -1,26 +1,31 @@
-import fs from 'fs'
+import fs from "fs";
 import { createConnection } from "mysql";
 
-var stock = fs.readFileSync('symbol_fetch/stock.json','utf8')
-stock = JSON.parse(stock)
+var stock = fs.readFileSync("symbol_fetch/stock.json", "utf8");
+stock = JSON.parse(stock);
 
 const conn = JSON.parse(fs.readFileSync("SoloData/SoloData.json"));
 
 let connection = createConnection(conn); // DB 커넥션 생성
 
 connection.connect((err) => {
-  if (err) {console.log("연결실패");
-  console.log(err);
-  }
-  else {
+  if (err) {
+    console.log("연결실패");
+    console.log(err);
+  } else {
     let create_sql = `CREATE TABLE stock ( 
       ${stock.columns.join(` VARCHAR(50),
       `)} VARCHAR(50)
       );`;
     connection.query(create_sql, function (err, results) {
       //create문이 에러면 로그 띄우기
-      if(err) console.log(err);
-      else console.log("stock 테이블 생성");
+      if (err) {
+        let del_sql = `DELETE FROM stock; `;
+        connection.query(del_sql, function (err, results) {
+          if (err) console.log(err);
+          else console.log("delete succesfully");
+        });
+      } else console.log("stock 테이블 생성");
       //테이블 생성,초기화 이후 데이터 저장
       var insert_sql = `INSERT INTO stock values ?;`;
       //db에 데이터 저장
@@ -33,7 +38,4 @@ connection.connect((err) => {
   }
 });
 
-
-
-
-export default {}
+export default {};
